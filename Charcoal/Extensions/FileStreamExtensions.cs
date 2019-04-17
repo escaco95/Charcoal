a317891d36d3446b717ec7c02e83dc2fd1861847
escaco95 <escaco95@naver.com>
2019-04-17 22:36:43 +0900

    익스텐션 작성

---
+++ Charcoal/Extensions/FileStreamExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Charcoal.Extensions
+{
+    public static class FileStreamExtensions
+    {
+        /// <summary>
+        /// <see cref="Int32"/>를 파일 스트림에 씁니다
+        /// </summary>
+        /// <param name="value">스트림에 쓸 <see cref="Int32"/> 값입니다</param>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        /// <exception cref="IOException"/>
+        /// <exception cref="ObjectDisposedException"/>
+        /// <exception cref="NotSupportedException"/>
+        public static void WriteInt32(this FileStream fs,Int32 value )
+        {
+            byte[] bytes = new byte[4]
+            {
+                (byte)(value >> 24),
+                (byte)(value >> 16),
+                (byte)(value >> 8),
+                (byte)value
+            };
+            fs.Write(bytes, 0, 4);
+        }
+    }
+}
---
+++ Charcoal/Extensions/Int32Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charcoal.Extensions
+{
+    public static class Int32Extensions
+    {
+        /// <summary>
+        /// 이 인스턴스의 숫자 값을 해당하는 바이트 배열 표현으로 변환합니다.
+        /// </summary>
+        public static byte[] ToByteArray(this Int32 value)
+        {
+            byte[] bytes = new byte[4]
+            {
+                (byte)(value >> 24),
+                (byte)(value >> 16),
+                (byte)(value >> 8),
+                (byte)value
+            };
+            return bytes;
+        }
+    }
+}
